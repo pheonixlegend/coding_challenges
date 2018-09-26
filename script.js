04250d381f31a659076f93d9501f53b79b16ca0e
pheonixlegend <jpnc2014@gmail.com>
2018-09-26 18:25:02 +0200

    Coding challenge 3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Coding Challenge number 1
+
 /*
 var johnHeight = 1.95;
 var markHeight = 1.69;
@@ -13,7 +15,11 @@ console.log("Is Mark's Bmi greater than John's Bmi? " + greaterBmi);
 
 */
 
-var teamJohn, teamMike;
+
+
+// Coding Challenge number 2
+
+/*var teamJohn, teamMike;
 var average, averageJohn, averageMike;
 
 averageJohn = (89 + 120 + 103) / 3;
@@ -29,4 +35,34 @@ else if (averageJohn < averageMike) {
 }
 else {
     console.log("It's a draw.")
+}*/
+
+
+
+
+// Coding Challenge number 3
+
+var bill, amount, tips, finalAmount;
+
+function calculateTip(bill) {
+    if (bill < 50) {
+        amount = 0.2 * bill;
+        return amount;
+    }
+
+    else if (bill >= 50 && bill <= 200) {
+        amount = 0.15 * bill;
+        return amount;
+    }
+
+    else if (bill > 200) {
+        amount = 0.1 * bill;
+        return amount;
+    }
 }
+
+tips = [calculateTip(124), calculateTip(48), calculateTip(268)];
+console.log(tips);
+
+finalAmount = ["$" + (tips[0] + 124), "$" + (tips[1] + 48), "$" + (tips[2] + 268)];
+console.log(finalAmount);
